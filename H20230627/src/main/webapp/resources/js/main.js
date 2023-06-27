184961c126d1e0620f110f1cc86e0535e00384fe
admin <admin@DESKTOP-KKACM4K>
2023-06-27 17:49:34 +0900

    add

--- H20230627/src/main/webapp/resources/js/main.js
+++ H20230627/src/main/webapp/resources/js/main.js
@@ -2,11 +2,39 @@
  * main.js 
  */
 
- console.log("main.js");
- 
- let obj ={name: "홍길동", age:21, phone: "010-1111-2222"}
- console.log(obj.name, obj['age']);
- for(let prop in obj) {
-	 console.log("속성: " + prop + ", 값: " + obj[prop]);
- }
-  
+console.log("main.js");
+
+let obj = { name: "홍길동", age: 21, phone: "010-1111-2222" }
+console.log(obj.name, obj['age']);
+for (let prop in obj) {
+	console.log("속성: " + prop + ", 값: " + obj[prop]);
+}
+
+let ary = []
+ary.push({ name: "김민수", age: 24, phone: "010-2222-3333" });
+ary.push(obj);
+ary.push("황영길");
+ary.pop();
+
+console.log(ary);
+
+for (let val of ary) {
+	for (let prop in val) {
+		console.log(val[prop]);
+	}
+}
+
+let ulTag = document.createElement('ul'); // <ul /> 태그
+for(let val of ary) {
+	let liTag = document.createElement('li'); // <li /> 태그만듬
+	liTag.innerText = val['name'];
+	ulTag.appendChild(liTag);
+	// button.
+	let btn =document.createElement('button'); // <button>클릭</button>
+	btn.innerText = '클릭';
+	liTag.appendChild(btn);
+}
+console.log(ulTag);
+
+document.getElementById('page-content-wrapper').appendChild(ulTag);
+
